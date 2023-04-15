e452de64b895897a76e9378a4291aad34d420113
Robert Smith <roberts@studiorms.com>
2023-04-15 15:27:10 -0700

    Add laravel ide helper

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -5,6 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Tweet
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $body
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User $user
+ * @method static \Database\Factories\TweetFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Tweet newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tweet newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tweet query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Tweet whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tweet whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tweet whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tweet whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Tweet whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Tweet extends Model
 {
     use HasFactory;
